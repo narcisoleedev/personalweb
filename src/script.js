ad61fce4b3aa407422ab0cc1edb014d52e70865a
tsuguil <narcisolee2002@usp.br>
2023-09-19 02:05:48 -0300

    added formatting and lint to the code

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    var tabs = document.getElementsByClassName('tabs-items')
-    for(let i=0; i<tabs.length; i++){
-        let element = tabs[i];
-        element.addEventListener('click', ()=>{
-            window.location.href = `./${element.innerText}`
-        })(element);
-    };
-})
+document.addEventListener("DOMContentLoaded", () => {
+  var tabs = document.getElementsByClassName("tabs-items");
+  for (let i = 0; i < tabs.length; i++) {
+    let element = tabs[i];
+    element.addEventListener("click", () => {
+      window.location.href = `./${element.innerText}`;
+    })(element);
+  }
+});
 
 /*document.addEventListener('DOMContentLoaded', ()=>{
     var tabs = document.getElementsByClassName('header-tabs')
